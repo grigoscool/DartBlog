d88c526e37d2ba48a0719797c47d6ce67b5e8c05
Sergey Grigorev <grigoscool@mail.ru>
2023-03-02 16:47:26 +0300

    add post list in template index

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('category/<str:slug>/', views.show_cat, name='cat'),
+    path('post/<str:slug>/', views.PostView.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,10 @@ from django.views import generic
 from .models import Post
 
 
-# def index(request):
-#     return render(request, 'blog/index.html')
-
 class IndexView(generic.ListView):
+    """
+    Home view - list of posts and main post.
+    """
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
@@ -17,6 +17,13 @@ class IndexView(generic.ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Classic Blog'
         return context
+
+
+class PostView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
 
 def show_cat(request, slug):
     return render(request, 'blog/cat.html')
