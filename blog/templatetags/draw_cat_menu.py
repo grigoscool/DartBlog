e0a772c8a5dc20342dcfed55a2be8897bf7930a6
Sergey Grigorev <grigoscool@mail.ru>
2023-03-02 12:36:25 +0300

    solve problem with class='active' in links

--- blog/templatetags/draw_cat_menu.py
+++ blog/templatetags/draw_cat_menu.py
@@ -5,7 +5,15 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag('blog/cat_menu.html')
-def get_cat_menu(menu_class=None):
+@register.inclusion_tag('blog/cat_menu.html', takes_context=True)
+def get_cat_menu(context, menu_class=None):
+    if check_home(str(context['request']).split('/')):
+        active_cat = 'home'
+    else:
+        active_cat = str(context['request']).split('/')[-2]
     cats = Category.objects.all()
-    return {'cats': cats, 'menu_class': menu_class}
+    return {'cats': cats, 'menu_class': menu_class, 'active_cat': active_cat}
+
+
+def check_home(s: list):
+    return False if 'category' in s else True
