ebb6875cbcc186f33a2d2650d800b470c586e0ec
Sergey Grigorev <grigoscool@mail.ru>
2023-03-03 16:07:57 +0300

    add sidebar posts

--- blog/templatetags/draw_sidebar_posts.py
+++ blog/templatetags/draw_sidebar_posts.py
@@ -8,5 +8,15 @@ register = template.Library()
 @register.inclusion_tag('blog/sidebar_posts.html', takes_context=True)
 def get_sidebar_posts(context, cnt=3):
     order = context['request'].GET
-    sidebar_posts = Post.objects.all()[:cnt]
-    return {'sidebar_posts': sidebar_posts}
+    for i in order:
+        if i == 'recent':
+            sidebar_posts = Post.objects.all().order_by('-create_at')[:cnt]
+            return {'sidebar_posts': sidebar_posts}
+        elif i == 'popular':
+            sidebar_posts = Post.objects.all().order_by('-views')[:cnt]
+            return {'sidebar_posts': sidebar_posts}
+        elif i == 'user':
+            sidebar_posts = Post.objects.all().order_by('author')[:cnt]
+            return {'sidebar_posts': sidebar_posts}
+
+
